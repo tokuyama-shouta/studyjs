c1f1751d61a59f4eea23b5cba49fd912644b186c
tokuyama shouta <toku1031ts@gmail.com>
2021-03-06 20:46:29 +0900

    let const

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-console.log("test");
+// console.log("test");
 // 出力
 
-alert("test");
+// alert("test");
 //アラートメッセージを表示する
+
+let x = 2;
+//書き換え可能
+x = 20;
+let y = 3;
+console.log(x + y);
+
+//HTMLのIDを取得する textContentでタグの中を表示
+document.getElementById("test").textContent = "test";
+
+//定数に値を入れる
+const tax = 1.1;
+const applePrice = 120;
+const orangePrice = 150;
+document.getElementById("apple").textContent = applePrice * tax;
+document.getElementById("orange").textContent = orangePrice * tax;
+
+
