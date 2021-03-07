c51db6d09131293c32d8fc619c179f374d2b8f42
tokuyama shouta <toku1031ts@gmail.com>
2021-03-07 11:04:34 +0900

    clickEvent

--- index.js
+++ index.js
@@ -4,20 +4,25 @@
 // alert("test");
 //アラートメッセージを表示する
 
-let x = 2;
+// let x = 2;
 //書き換え可能
-x = 20;
-let y = 3;
-console.log(x + y);
+// x = 20;
+// let y = 3;
+// console.log(x + y);
 
 //HTMLのIDを取得する textContentでタグの中を表示
-document.getElementById("test").textContent = "test";
+// document.getElementById("test").textContent = "test";
 
 //定数に値を入れる
-const tax = 1.1;
-const applePrice = 120;
-const orangePrice = 150;
-document.getElementById("apple").textContent = applePrice * tax;
-document.getElementById("orange").textContent = orangePrice * tax;
+// const tax = 1.1;
+// const applePrice = 120;
+// const orangePrice = 150;
+// document.getElementById("apple").textContent = applePrice * tax;
+// document.getElementById("orange").textContent = orangePrice * tax;
 
 
+//取得したIDをクリックするとイベントを起こす処理
+document.getElementById("test").addEventListener('click', function() {
+  //thisは↑の事をさす
+  this.textContent = 'クリックされた。'
+});
