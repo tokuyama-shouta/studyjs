d0892efbede4e5c7d82de15fd568c6e5f8812bc9
tokuyama shouta <toku1031ts@gmail.com>
2021-03-14 21:36:48 +0900

    commit

--- index.js
+++ index.js
@@ -124,18 +124,18 @@
 // }
 
 
-const tax = 1.1;
-function insertText(itemPrice,element) {
-  function calculation(price) {
-    return Math.floor(price * tax);
+// const tax = 1.1;
+// function insertText(itemPrice,element) {
+//   function calculation(price) {
+//     return Math.floor(price * tax);
     
-  }
-  document.getElementById(element).textContent = itemPrice + "円の商品の税込み価格は" + calculation(itemPrice) + "円です。";
+//   }
+//   document.getElementById(element).textContent = itemPrice + "円の商品の税込み価格は" + calculation(itemPrice) + "円です。";
 
-}
-insertText(100,"result");
-insertText(200,"result2");
-insertText(300,"result3");
+// }
+// insertText(100,"result");
+// insertText(200,"result2");
+// insertText(300,"result3");
 
 
 
