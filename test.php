0b6e52b2d379e44b86f375531169a22ce02caa60
tokuyama shouta <toku1031ts@gmail.com>
2021-03-23 21:11:41 +0900

    php study

--- test.php
+++ test.php
@@ -1,1 +1,28 @@
-<?php echo helloworld?> 
+<?php 
+
+echo "helloworld"; 
+
+//変数
+//ブログのタイトル
+$title = "PHPテスト";
+$content = 'PHPテストです。';
+$post_at = '2020/03/23';
+$tag = ['PHP','プログラミング'];
+$status = true;
+
+//定数
+const ID = 1;
+echo ID;
+echo '<br>';
+echo $title;
+echo '<br>';
+echo $content;
+echo '<br>';
+echo $post_at;
+echo '<br>';
+print_r($tag);
+
+//データ型
+ver_dump(ID);
+
+?> 
