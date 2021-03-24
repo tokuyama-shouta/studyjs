ecc6a3d4c572d7449dfb4625ab7b0a9c6f8d3f6e
tokuyama shouta <toku1031ts@gmail.com>
2021-03-24 21:53:24 +0900

    配列

--- test.php
+++ test.php
@@ -1,28 +1,65 @@
 <?php 
 
-echo "helloworld"; 
+// echo "helloworld"; 
 
 //変数
 //ブログのタイトル
+const ID = 1;
 $title = "PHPテスト";
 $content = 'PHPテストです。';
 $post_at = '2020/03/23';
 $tag = ['PHP','プログラミング'];
 $status = true;
 
+const ID = 2;
+$title2 = "PHPテスト";
+$content2 = 'PHPテストです。';
+$post_at2 = '2020/03/23';
+$tag2 = ['PHP','プログラミング'];
+$status2 = true;
+
+$blog1 = array(
+  'id' => ID,
+  'title' => $title,
+  'content' => $content,
+  'post_at' => $post_at,
+  'tag' => $tag,
+  'status' => $status,
+);
+
+$blog2 = array(
+  'id2' => ID2,
+  'title2' => $title2,
+  'content2' => $content2,
+  'post_at2' => $post_at2,
+  'tag2' => $tag2,
+  'status2' => $status2,
+);
+echo '<pre>';
+var_dump($blog2);
+echo '</pre>';
+// echo $blog1['title'];
+
+$blogs = [$blog1,$blog2];
+
+echo '<pre>';
+var_dump($blogs);
+echo '</pre>';
+
 //定数
-const ID = 1;
-echo ID;
-echo '<br>';
-echo $title;
-echo '<br>';
-echo $content;
-echo '<br>';
-echo $post_at;
-echo '<br>';
-print_r($tag);
+// echo ID;
+// echo '<br>';
+// echo $title;
+// echo '<br>';
+// echo $content;
+// echo '<br>';
+// echo $post_at;
+// echo '<br>';
+// print_r($tag);
 
 //データ型
-ver_dump(ID);
+// ver_dump(ID);
+
+
 
 ?> 
