fa7826de272239228e1ba9757304a7cab7c09bf2
tokuyama shouta <toku1031ts@gmail.com>
2021-04-03 21:12:58 +0900

    デフォルト

--- index.js
+++ index.js
@@ -312,7 +312,10 @@
 // const {name, age} = myProfile;
 // const message2 = `名前は${name}です。年は${age}歳です。`;
 
-const myProfile = ["pkpk",25];
-const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
-const [name,age] = myProfile;
-const message4 = `名前は${name}です。年齢は${age}歳です。`;
+// const myProfile = ["pkpk",25];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}歳です。`;
+// const [name,age] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}歳です。`;
+
+const sayHello = (name = "pkpk") => console.log(`こんにちは!${name}さん！`);
+sayHello('pkpk');
