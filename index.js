e8cf411795f4c4bc09a98dc5ec4c7be51fbf2892
tokuyama shouta <toku1031ts@gmail.com>
2021-04-03 21:21:09 +0900

    スプレッド構文

--- index.js
+++ index.js
@@ -317,5 +317,39 @@
 // const [name,age] = myProfile;
 // const message4 = `名前は${name}です。年齢は${age}歳です。`;
 
-const sayHello = (name = "pkpk") => console.log(`こんにちは!${name}さん！`);
-sayHello('pkpk');
+// const sayHello = (name = "pkpk") => console.log(`こんにちは!${name}さん！`);
+// sayHello('pkpk');
+
+//スプレッド構文 ...
+
+//配列の展開
+// const arr1 = [1,2];
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+//まとめる
+const arr2 = [1,2,3,4,5];
+const [num1, num2, ...arr3 ] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+//配列のコピーや結合
+const arr4 = [10,20];
+const arr5 = [30,40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+
+
+
+
