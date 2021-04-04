16384f6d4c032efccf9f891e7c3e7494f29efba4
tokuyama shouta <toku1031ts@gmail.com>
2021-04-04 15:55:58 +0900

    論理演算子

--- index.js
+++ index.js
@@ -386,10 +386,37 @@
 // const formattNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください。';
 // console.log(formattedNum);
 
-const checkSum = (num1,num2) => {
-  return num1 + num2 > 100 ? '100を超えています。' : '許容範囲内です。'
-}
-console.log(checkSum(50,40));
+// const checkSum = (num1,num2) => {
+//   return num1 + num2 > 100 ? '100を超えています。' : '許容範囲内です。'
+// }
+// console.log(checkSum(50,40));
+
+//論理演算子の本当の意味
+
+// const flag1 = true;
+// const flag2 = false;
+
+// if(flag1 || flag2) {
+//   console.log("1か2はtrueになります。");
+  
+// }
+// if(flag1 && flag2) {
+//   console.log("1も2もtrueになります。");
+  
+// }
+
+// || は左側がfalseなら右側を返す。
+
+// const num = null;
+// const fee = num || "金額未設定です。";
+// console.log(fee);
+
+// && は左側がtrueなら右側を返す。
+const num2 = 100;
+const fee2 = num2 && "何か設定されました。";
+console.log(fee2);
+
+
 
 
 
