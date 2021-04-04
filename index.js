c17b5f49ceb4df3562602a1ff1465d96f3937785
tokuyama shouta <toku1031ts@gmail.com>
2021-04-04 15:45:57 +0900

    参考演算子

--- index.js
+++ index.js
@@ -349,7 +349,7 @@
 // const arr7 = [...arr4, ...arr5];
 // console.log(arr7);
  
-const nameArr = ["田中","山田","pkpk"];
+// const nameArr = ["田中","山田","pkpk"];
 // for (let index = 0; index < nameArr.length; index++){
 //   console.log(`${index + 1}番目は${nameArr[index]}`);
 // }
@@ -366,14 +366,34 @@ const nameArr = ["田中","山田","pkpk"];
 // });
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "pkpk") {
-    return name;
-  } else {
-    return `${name}さん`
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "pkpk") {
+//     return name;
+//   } else {
+//     return `${name}さん`
+//   }
+// });
+// console.log(newNameArr);
+
+//三項演算子
+//ある条件？条件がtrueの時：条件がfalseの時
+// const val1 = 1 > 0 ? 'trueです' : 'falseです。'
+// console.log(val1);
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください。';
+// console.log(formattedNum);
+
+const checkSum = (num1,num2) => {
+  return num1 + num2 > 100 ? '100を超えています。' : '許容範囲内です。'
+}
+console.log(checkSum(50,40));
+
+
+
+
 
 
 
