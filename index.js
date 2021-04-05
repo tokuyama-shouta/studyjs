c1b35cdfe2b12653d71753db1e2be43a2d47ec30
tokuyama shouta <toku1031ts@gmail.com>
2021-04-05 21:02:18 +0900

    追加機能

--- index.js
+++ index.js
@@ -417,7 +417,28 @@
 // console.log(fee2);
 
 
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する。
+  const inputText = document.getElementById("add-text").nodeValue;
+  document.getElementById("add-text").value = "";
 
+  //divタグを生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  
+  //divタグの子要素に各要素を設定
+  div.appendChild(li);
+  
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+  
+}
+
+document.getElementById("add-button").addEventListener("click", () => onClickAdd());
 
 
 
