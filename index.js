6e7dcb5fabad6ebd5ccc53068ca2f583f4a6bab1
tokuyama shouta <toku1031ts@gmail.com>
2021-04-06 20:47:33 +0900

    追加機能ボタン

--- index.js
+++ index.js
@@ -429,9 +429,27 @@ const onClickAdd = () => {
   //liタグ
   const li = document.createElement("li");
   li.innerText = inputText;
+
+  //button 完了
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click",() => {
+    alert();
+  });
+
+
+  //button 削除
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //押された削除ボタンの親タグを未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+  })
   
   //divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   
   //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
